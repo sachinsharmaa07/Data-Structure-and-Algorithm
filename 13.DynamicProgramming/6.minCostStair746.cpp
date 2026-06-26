7d14b465e38c0e2f5c1354b479b711c1cdae0a9c
Sachin Sharma <sachinsharma@BatMobile.local>
2026-06-26 15:05:51 +0530

    some messages

--- 13.DynamicProgramming/6.minCostStair746.cpp
+++ 13.DynamicProgramming/6.minCostStair746.cpp
@@ -10,4 +10,15 @@ using namespace std;
         dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
      }
     cout<< min(dp[n-1],dp[n-2]);
+    //or space optimization
+    
+   int  prev2=cost[0];
+    int prev1=cost[1];
+   int  curr=0;
+     for(int i=2;i<n;i++){
+        curr=cost[i]+min(prev1,prev2);
+        prev2=prev1;
+        prev1=curr;
+     }
+    cout<<min(prev1,prev2);
     }
